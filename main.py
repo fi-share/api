420d18b055bdff40abf024280cd464a9fda06582
Brayan Torres <btorres@fi.uba.ar>
2024-07-16 22:22:12 -0300

    Se agregaron todos los endpoints de repositorios [POST,DELETE,PATH]

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 from models import db, Materias, Cursos, Tps, Repositorios
+from datetime import datetime
 from config import Config
 
 
@@ -12,6 +13,11 @@ db.init_app(app)
 @app.errorhandler(500)
 def internal_server_error(e):
     return jsonify(error=str(e)), 500
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify(error=str(e)), 400
 
 
 @app.errorhandler(404)
@@ -145,6 +151,116 @@ def get_tp_repositorios(id_tp):
     except Exception:
         abort(500, description="Internal Server Error")
 
+
+"""
+PRE: Se espera un JSON con los datos del repositorio y un id_tp válido en la URL.
+POST: Devuelve un JSON con los datos del repositorio creado o un mensaje de error.
+"""
+
+
+@app.route('/tps/<int:id_tp>/repositorios', methods=['POST'])
+def compartir_public_repository(id_tp):
+
+    try:
+        tp = Tps.query.get(id_tp)
+        if tp is None:
+            abort(404, description="Resource not found")
+
+        data = request.json
+        if not data:
+            abort(400, description="Bad Request")
+
+        columns_name = ['full_name', 'descripcion', 'id_usuario']
+        for name in columns_name:
+            if name not in data:
+                abort(400, description="Bad Request")
+
+        nuevo_repositorio = Repositorios(
+            full_name=data['full_name'],
+            descripcion=data['descripcion'],
+            calificacion=data.get('calificacion', 0),
+            id_usuario=data['id_usuario'],
+            fecha_creacion=datetime.now(),
+            id_tp=id_tp
+        )
+
+        db.session.add(nuevo_repositorio)
+        db.session.commit()
+
+        response_data = {
+            'id': nuevo_repositorio.id,
+            'full_name': nuevo_repositorio.full_name,
+            'descripcion': nuevo_repositorio.descripcion,
+            'calificacion': nuevo_repositorio.calificacion,
+            'id_usuario': nuevo_repositorio.id_usuario,
+            'fecha_creacion': nuevo_repositorio.fecha_creacion.isoformat(),
+            'id_tp': nuevo_repositorio.id_tp
+        }
+
+        return jsonify(response_data), 201
+
+    except Exception:
+        db.session.rollback()
+        abort(500, description="Internal Server Error")
+
+
+"""
+Para metodos PATH o DELETE se espera un id_tp y un id_repositorio válidos en la URL...
+
+PRE: Se espera un JSON con los datos a actualizar, un id_tp y un id_repositorio válidos en la URL.
+POST: Devuelve un JSON con los datos actualizados del repositorio o un mensaje de error.
+
+"""
+
+
+@app.route('/tps/<int:id_tp>/repositorios/<int:id_repositorio>', methods=['PATCH', 'DELETE'])
+def up_and_delete_repository(id_tp, id_repositorio):
+
+    try:
+        tp = Tps.query.get(id_tp)
+        if tp is None:
+            abort(404, description="Resource not found")
+
+        repositorio = Repositorios.query.filter_by(id=id_repositorio, id_tp=id_tp).first()
+        if repositorio is None:
+            abort(404, description="Resource not found")
+
+        if request.method == 'DELETE':
+            db.session.delete(repositorio)
+            db.session.commit()
+
+            return jsonify({'message': f'Repositorio con ID {id_repositorio} eliminado exitosamente'}), 200
+
+        data = request.json
+        if not data:
+            abort(400, description="Bad Requests")
+
+        if 'full_name' in data:
+            repositorio.full_name = data['full_name']
+        if 'descripcion' in data:
+            repositorio.descripcion = data['descripcion']
+        if 'calificacion' in data:
+            repositorio.calificacion = data['calificacion']
+        if 'id_usuario' in data:
+            repositorio.id_usuario = data['id_usuario']
+
+        db.session.commit()
+
+        response_data = {
+            'id': repositorio.id,
+            'full_name': repositorio.full_name,
+            'descripcion': repositorio.descripcion,
+            'calificacion': repositorio.calificacion,
+            'id_usuario': repositorio.id_usuario,
+            'fecha_creacion': repositorio.fecha_creacion.isoformat(),
+            'id_tp': repositorio.id_tp
+        }
+
+        return jsonify(response_data), 200
+
+    except Exception:
+        db.session.rollback()
+        abort(500, description="Internal Server Error")
 
 if __name__ == "__main__":
     print("Starting server...")
